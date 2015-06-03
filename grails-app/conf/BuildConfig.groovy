d51e303bd02fecc375a2f138205d19912269e005
rsanz <roberto.sanz@ticketbis.com>
2015-06-03 12:45:45 +0200

    Adding coverage plugin and set test fork mode

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -7,7 +7,7 @@ grails.project.fork = [
     //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
 
     // configure settings for the test-app JVM, uses the daemon by default
-    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    test: false,
     // configure settings for the run-app JVM
     run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
     // configure settings for the run-war JVM
@@ -40,6 +40,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
+        test ":code-coverage:2.0.3-3"
         build(":release:3.0.1",
               ":rest-client-builder:1.0.3") {
             export = false
